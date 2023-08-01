c4de8c9adf096c63dd3f9ef044f6376b65d7f6e6
Mikalai Alkhimenka <alkhimenkamikalai90@gmail.com>
2023-08-01 08:41:45 +0200

    Solved

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int center;
+        if (sideLength % 2 == 0) {
+            center = sideLength / 2;
+        } else {
+            center = (sideLength + 1) / 2;
+        }
+            for (int i = 1; i <= sideLength; i++) {
+                for (int j = 1; j <= sideLength; j++) {
+                    if (i == center || j == center) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
 
+            }
     }
+
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,36 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
-    }
+        int top;
+        int bottom = height / 2 - 1;
+        if (height % 2 != 0) {
+            top = height / 2 + 1;
+        } else {
+            top = height / 2;
+        }
+        for (int i = 0; i < top; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < height - 2 * i; j++) {
+                System.out.print(8);
+            }
+            for (int j = height; j >height - i  ; j--) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = bottom; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < height - 2 * i; j++) {
+                System.out.print(8);
+            }
+            for (int j = height; j >height - i  ; j--) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        }
 }
